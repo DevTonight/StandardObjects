86c696169084036c99736053e5df0974259b4de4
Matthew Cook <matthewcook0485@centurylink.net>
2015-03-22 00:15:09 -0500

    Added the Item class.

---
+++ StandardObjects/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StandardObjects
+{
+	/// <summary>
+	/// The Item class will is a generic class that can be used to describe
+	/// physical objects, documents, or any other entity.  For example,
+	/// an Item can be a Product or be a part of a Product.
+	/// </summary>
+	/// 03/22/2015 - Added ItemID, Name, and Description properties.
+	public class Item
+	{
+		public Guid ItemID {get;set;}
+		public string Name {get;set;}
+		public string Description {get;set;}
+	}
+}
+
