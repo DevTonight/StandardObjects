bfc6d791ad9915fc6ab646ba7ed3022491598f1e
Matthew Cook <matthewcook0485@centurylink.net>
2015-03-22 10:17:49 -0500

    Added Student and Assingment classes.

---
+++ StandardObjects/Education/Assignment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StandardObjects.Education
+{
+	public class Assignment
+	{
+		public int AssignmentID { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public double Weight { get; set; }
+
+		public double Score { get; set; }
+
+		public DateTime DateAssigned { get; set; }
+
+		public DateTime DueDate { get; set; }
+
+		public DateTime DateTurnedIn { get; set; }
+
+		public Assignment (int assingmentID, string name, string description, double weight, double score)
+		{
+			this.AssignmentID = assingmentID;
+			this.Name = name;
+			this.Description = description;
+			this.Weight = weight;
+			this.Score = score;
+		}
+	}
+}
+
---
+++ StandardObjects/Education/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StandardObjects.Education
+{
+	public class Student
+	{
+		public int StudentID { get; set; }
+
+		public string FirstName { get; set; }
+
+		public char MiddleInitial { get; set; }
+
+		public string LastName { get; set; }
+
+		public char Gender { get; set; }
+
+		public HashSet<Assignment> Assignments{ get; set; }
+
+		public Student (int studentID, string firstName, char middleInitial, string lastName, char gender)
+		{
+			this.StudentID = studentID;
+			this.FirstName = firstName;
+			this.MiddleInitial = middleInitial;
+			this.LastName = lastName;
+			this.Gender = gender;
+			this.Assignments = new HashSet<Assignment> ();
+
+		}
+	}
+}
+
